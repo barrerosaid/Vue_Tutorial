19d7016be92592062bc664302a96d1fcf70a8d82
Barrero <said.barrero@hp.com>
2020-01-09 01:31:09 -0700

    adding todo list basics before adding functionality like a checkmark

--- list/src/App.vue
+++ list/src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Todos v-bind:todos="todos" /> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+	Todos
+  },
+  data(){
+	return {
+		todos:[
+			{
+				id: 1,
+				title: "Todo Item 1",
+				completed: false
+			
+			},
+			{
+				id: 2,
+				title: "Todo Item 2",
+				completed: true
+			
+			},
+			{
+				id: 3,
+				title: "Todo Item 3",
+				completed: false
+			
+			}
+		]
+	}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
